69d544030d479a8b7d0c8d6ac0655de75a65c4d1
zEttOn86 <38281934+zEttOn86@users.noreply.github.com>
2018-10-02 10:07:41 +0900

    Add updater.py

---
+++ updater.py
@@ -0,0 +1,30 @@
+#coding:utf-8
+import os, sys, time
+import numpy as np
+import chainer
+import chainer.links as L
+import chainer.functions as F
+
+class EspcnUpdater(chainer.training.StandardUpdater):
+    def __init__(self, *args, **kwargs):
+        self.gen = kwargs.pop('model')
+        super(EspcnUpdater, self).__init__(*args, **kwargs)
+
+    def mse_loss(self, y, t):
+        loss = F.mean_squared_error(y, t)
+        return loss
+
+    def update_core(self):
+        gen_optimizer = self.get_optimizer('gen')
+
+        batch = self.get_iterator("main").next()#iterator
+        x, t = self.converter(batch, self.device)
+
+        gen = self.gen
+        y = gen(x)
+
+        loss_gen = self.mse_loss(y, t)
+        gen.cleargrads()
+        loss_gen.backward()
+        gen_optimizer.update()
+        chainer.reporter.report({'loss' : loss_gen})
